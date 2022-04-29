aa56cd35c30b7ae9ba3a420824f992cd8220749c
Darius Seals <dariusseals@dariuss-mbp.myfiosgateway.com>
2022-04-28 20:48:52 -0400

    added navbar styling, and svg logo for nav

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,15 +1,18 @@
+import './navigation.styles.scss';
 import { Outlet, Link } from 'react-router-dom';
 import { Fragment } from 'react';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
 const Navigation = () => (
     <Fragment>
-      <div className='navigation'>
+      <nav className='navigation'>
         <Link className='logo-container' to="/">
-            <div>Logo</div>
+            <Logo />
         </Link>
         <div className='nav-links-container'>
             <Link className="nav-link" to='shop'>SHOP</Link>
         </div>
-      </div>
+      </nav>
       <Outlet />
     </Fragment>
   );
