5dd29a5cb703cb13f391f45dc9b8b082f3423165
Darius Seals <tristarmediagroupinc@gmail.com>
2022-09-12 14:28:43 -0400

    categories saga migration from categories thunk

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
-import { fetchCategoriesAsync } from '../../store/categories/categories.action';
+import { fetchCategoriesStart } from '../../store/categories/categories.action';
 import { useDispatch } from 'react-redux';
 
 
@@ -11,7 +11,7 @@ const  Shop = () => {
 
   //maybe remove dispatch from dependency array?
   useEffect(() => {
-        dispatch(fetchCategoriesAsync())
+        dispatch(fetchCategoriesStart())
   },[dispatch]);
 
   return (
