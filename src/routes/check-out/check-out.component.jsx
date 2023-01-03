809ff7fbcbbd88b9eba6f6ddfe35604cd23bb10c
Darius Seals <darius.seals@sas.com>
2023-01-03 18:30:06 -0500

    setup stripe payment form and minimal styling.  Also set up payment form component

--- src/routes/check-out/check-out.component.jsx
+++ src/routes/check-out/check-out.component.jsx
@@ -1,44 +1,51 @@
-import React from 'react';
-import {CheckoutContainer, CheckoutHeader, HeaderBlock, Total} from  './check-out.styles.jsx'
+import React from "react";
+import {
+  CheckoutContainer,
+  CheckoutHeader,
+  HeaderBlock,
+  Total,
+} from "./check-out.styles.jsx";
 //import { useContext } from 'react';
 //import { CartContext } from '../../context/cart.context';
-import CheckoutItem from '../../components/checkout-item/checkout-item.component';
-import { selectCartTotal, selectCartItems } from '../../store/cart/cart.selector.js';
-import { useSelector } from 'react-redux';
-
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import {
+  selectCartTotal,
+  selectCartItems,
+} from "../../store/cart/cart.selector.js";
+import { useSelector } from "react-redux";
+import PaymentForm from "../../components/payment-form/payment-form.component.jsx";
 
 const CheckOut = () => {
-    //const { cartItems, cartTotal} = useContext(CartContext);
-    const cartItems = useSelector(selectCartItems);
-    const cartTotal = useSelector(selectCartTotal)
-  
-  return (
-    <CheckoutContainer className='checkout-container'>
-      <CheckoutHeader className='checkout-header'>
-          <HeaderBlock className='header-block'>
-            <span>Product</span>
-          </HeaderBlock>
-          <HeaderBlock>
-            <span>Description</span>
-          </HeaderBlock>
-          <HeaderBlock>
-            <span>Quantity</span>
-          </HeaderBlock>
-          <HeaderBlock>
-            <span>Price</span>
-          </HeaderBlock>
-          <HeaderBlock>
-            <span>Remove</span>
-          </HeaderBlock>
-      </CheckoutHeader>
-      {
-          cartItems.map((cartItem)=> (
-              <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-          ))
-      }
-      <Total>Total: ${cartTotal}</Total>
-    </CheckoutContainer>
-  )
-}
+  //const { cartItems, cartTotal} = useContext(CartContext);
+  const cartItems = useSelector(selectCartItems);
+  const cartTotal = useSelector(selectCartTotal);
 
-export default CheckOut
+  return (
+    <CheckoutContainer className="checkout-container">
+      <CheckoutHeader className="checkout-header">
+        <HeaderBlock className="header-block">
+          <span>Product</span>
+        </HeaderBlock>
+        <HeaderBlock>
+          <span>Description</span>
+        </HeaderBlock>
+        <HeaderBlock>
+          <span>Quantity</span>
+        </HeaderBlock>
+        <HeaderBlock>
+          <span>Price</span>
+        </HeaderBlock>
+        <HeaderBlock>
+          <span>Remove</span>
+        </HeaderBlock>
+      </CheckoutHeader>
+      {cartItems.map((cartItem) => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+      <Total>Total: ${cartTotal}</Total>
+      <PaymentForm />
+    </CheckoutContainer>
+  );
+};
+
+export default CheckOut;
